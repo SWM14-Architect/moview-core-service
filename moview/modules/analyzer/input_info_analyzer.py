6360f5dd265eee7dd45f0abed62ad6f72a738237
hys <ssfic3380@gmail.com>
2023-07-07 12:56:02 +0900

    refactor: import 경로 수정 및 class로 리팩토링, test 1종 추가

--- moview/modules/analyzer/input_info_analyzer.py
+++ moview/modules/analyzer/input_info_analyzer.py
@@ -5,42 +5,57 @@ from langchain.prompts.chat import (
     HumanMessagePromptTemplate
 )
 
-from mvp.data_manager import *
-from mvp.util import remove_indent
-from typing import *
+from moview.utils.data_manager import *
+from moview.utils.util import remove_indent
 
 
-def input_info_analyzer(
-    data_manager: DataManager,
-    evaluation_manager: EvaluationManager
-):
-    chat_manager = ChatManager()
+class InputInfoAnalyzer:
+    """
+    회사의 모집공고와 직무 정보를 토대로 면접자의 자기소개서와 1분 자기소개를 분석하여 면접관으로써 좋은 점과 아쉬운 점을 분석하여 출력합니다.
+    """
+    def __init__(self, data_manager: DataManager, evaluation_manager: EvaluationManager):
+        self.data_manager = data_manager
+        self.evaluation_manager = evaluation_manager
 
-    prompt = ChatPromptTemplate(
-        messages=[
-            SystemMessagePromptTemplate.from_template(
-                remove_indent(
-                    f"""You are an interviewer at {data_manager.company}.
+    def analyze_input_info(self, chat_manager: ChatManager):
+        """
+        Args:
+            chat_manager: OpenAI의 API를 사용하기 위한 ChatManager 객체
 
-                    {data_manager.get_userdata()}
-                    """)),
+        Returns:
+            None
+        """
+        chat_manager = ChatManager()
 
-            HumanMessagePromptTemplate.from_template(
-                remove_indent(
-                    """As an interviewer, Please read the cover letter and self-introduction of the interviewee and provide an evaluation from the {company}'s perspective, dividing it into positive aspects and areas for improvement. Please write in Korean. The format is as follows:
+        # 면접자의 자기소개서와 1분 자기소개를 분석하기 위한 프롬프트를 작성합니다.
+        prompt = ChatPromptTemplate(
+            messages=[
+                SystemMessagePromptTemplate.from_template(
+                    remove_indent(
+                        f"""You are an interviewer at {self.data_manager.company}.
 
-                    "좋은점":
-                    - Content of positive aspects
-                    "아쉬운점":
-                    - Content of areas for improvement
+                            {self.data_manager.get_userdata()}
+                            """)),
 
-                    Please write in Korean.
-                    """))
-        ],
-        input_variables=["company"],
-    )
+                HumanMessagePromptTemplate.from_template(
+                    remove_indent(
+                        """As an interviewer, Please read the cover letter and self-introduction of the interviewee and provide an evaluation from the {company}'s perspective, dividing it into positive aspects and areas for improvement. Please write in Korean. The format is as follows:
 
-    create_question_chain = LLMChain(llm=chat_manager.get_chat_model(),
-                                     prompt=prompt)
-    output = create_question_chain(data_manager.company)
-    evaluation_manager.add_coverletter_evaluation(output)
+                        "좋은점":
+                        - Content of positive aspects
+                        "아쉬운점":
+                        - Content of areas for improvement
+
+                        Please write in Korean.
+                        """))
+            ],
+            input_variables=["company"],
+        )
+
+        # 작성된 프롬프트를 이용하여 chain을 생성하고 실행합니다.
+        create_question_chain = LLMChain(llm=chat_manager.get_chat_model(),
+                                         prompt=prompt)
+        output = create_question_chain(self.data_manager.company)
+
+        # 분석 결과를 EvaluationManager에 저장합니다.
+        self.evaluation_manager.add_coverletter_evaluation(output['text'])
--- moview/modules/prompter/question_prompter.py
+++ moview/modules/prompter/question_prompter.py
@@ -1,16 +1,31 @@
-from mvp.data_manager import *
+from moview.utils.data_manager import *
 
 
-def question_prompter(
-    question_entity: QuestionEntity,
-):
-    question = question_entity.question
+class QuestionPrompter:
+    """
+    질문을 던지고, 답변을 받아서 QuestionEntity에 저장합니다.
+    """
 
-    # TODO: 나중에 면접자의 답변을 프론트에서 받아와야함.
-    print(f"면접관: {question}")
-    answer = input("면접자:  ...질문에 답변하세요.\n")
-    question_entity.add_answer(answer)
+    def __init__(self, data_manager: DataManager):
+        self.data_manager = data_manager
 
-    if answer in ["exit", "c"]:
-        return True
-    return False
+    def prompt_question(self, question_entity: QuestionEntity) -> bool:
+        """
+        Args:
+            question_entity: 면접자가 받았던 질문(question_entity.question)과 면접자의 답변(question_entity.answer) 쌍을 담고 있는 객체
+
+        Returns:
+            True: 종료
+            False: 계속 진행
+        """
+        question = question_entity.question
+
+        # TODO: 나중에 면접자의 답변을 프론트에서 받아와야함.
+        print(f"면접관: {question}")
+        answer = input("면접자:  ...질문에 답변하세요.\n")
+        question_entity.add_answer(answer)
+
+        # 입력이 "exit" 또는 "c"이면 종료 (True 반환)
+        if answer in ["exit", "c"]:
+            return True
+        return False
