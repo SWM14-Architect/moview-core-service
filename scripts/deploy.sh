4f800c7348f343f991df4eed379ea6f40d49612e
ssfic3380 <ssfic3380@naver.com>
2023-07-11 17:59:50 +0900

    feat: Anaconda 가상환경 대신 venv 가상환경을 사용하도록 변경

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 REPOSITORY=/home/ubuntu/app
+VENV=$REPOSITORY/venv
 
 echo ">>> 애플리케이션 디렉토리로 이동합니다."
 cd $REPOSITORY
 
-echo ">>> Anaconda 가상환경(moview)을 활성화합니다."
-source /home/ubuntu/miniconda/etc/profile.d/conda.sh
-conda activate moview
+echo ">>> Python 가상환경(venv)을 활성화합니다."
+source $VENV/bin/activate
 
 echo ">>> Flask 앱을 실행합니다."
 nohup python app.py > app.log 2>&1 &
