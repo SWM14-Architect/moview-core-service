56055517d585f68b27a79bcfe6e5c59e45b51930
yang-yeongjun <dasd412@naver.com>
2023-07-13 14:32:45 +0900

    feat : AnswerEvaluation class 로그 추가 및 테스트 완료.

--- moview/controller/evaluation_controller.py
+++ moview/controller/evaluation_controller.py
@@ -15,7 +15,6 @@ api = Namespace('evaluation', description='evaluation api')
 class UserDataUpload(Resource):
     @api.doc("회사이름, 직군, 모집공고, 자기소개서를 입력받습니다.")
     def post(self):
-
         # todo : request body 한글로 들어올 경우, 로그에서 한글이 안나옵니다. 추후 문제 해결 필요.
         request_body = request.get_json()
         log = {"time": str(datetime.datetime.now()), "message": "Start of the UserDataUpload",
@@ -38,7 +37,13 @@ class UserEvaluation(Resource):
     def get(self):
         chat_manager = ChatManager(streaming=True, temperature=0)
 
+        log = {"time": str(datetime.datetime.now()), "message": "Start of UserEvaluation"}
+        write_log_in_txt(log, UserEvaluation.__name__)
+
         if check_manager("data_manager"):
+            log = {"time": str(datetime.datetime.now()), "message": "Data manager check failed"}
+            write_log_in_txt(log, UserEvaluation.__name__)
+
             return get_manager_error_response("data_manager")
 
         data_manager = DataManager()
@@ -48,15 +53,25 @@ class UserEvaluation(Resource):
             response = InputInfoAnalyzer(data_manager, evaluation_manager) \
                 .analyze_input_info(chat_manager)
         except openai.OpenAIError as e:
+
+            log = {"time": str(datetime.datetime.now()), "message": f"OpenAIError: {e}"}
+            write_log_in_txt(log, UserEvaluation.__name__)
+
             return make_response(
                 jsonify({"messages": f"OpenAI API error\n{e}"}),
                 HTTPStatus.REQUEST_TIMEOUT
             )
         except Exception as e:
+
+            log = {"time": str(datetime.datetime.now()), "message": f"Error: {e}"}
+            write_log_in_txt(log, UserEvaluation.__name__)
+
             return make_response(
                 jsonify({"messages": f"error {e}"}),
                 HTTPStatus.INTERNAL_SERVER_ERROR
             )
+        log = {"time": str(datetime.datetime.now()), "message": "End of the UserEvaluation"}
+        write_log_in_txt(log, UserEvaluation.__name__)
 
         session['evaluation_manager'] = evaluation_manager.get_all_evaluation()
         return make_response(
@@ -75,10 +90,20 @@ class AnswerEvaluation(Resource):
             request_body['answer']
         )
 
+        log = {"time": str(datetime.datetime.now()), "message": "Start of the AnswerEvaluation",
+               "request_body": request_body}
+        write_log_in_txt(log, AnswerEvaluation.__name__)
+
         if check_manager("data_manager"):
+            log = {"time": str(datetime.datetime.now()), "message": "Data manager check failed"}
+            write_log_in_txt(log, AnswerEvaluation.__name__)
+
             return get_manager_error_response("data_manager")
 
         if check_manager("evaluation_manager"):
+            log = {"time": str(datetime.datetime.now()), "message": "Evaluation manager check failed"}
+            write_log_in_txt(log, AnswerEvaluation.__name__)
+
             return get_manager_error_response("evaluation_manager")
 
         data_manager = DataManager()
@@ -90,11 +115,19 @@ class AnswerEvaluation(Resource):
             response = AnswerAnalyzer(data_manager, question_entity, evaluation_manager) \
                 .analyze_answer(ChatManager())
         except openai.OpenAIError as e:
+
+            log = {"time": str(datetime.datetime.now()), "message": f"OpenAIError: {e}"}
+            write_log_in_txt(log, AnswerEvaluation.__name__)
+
             return make_response(
                 jsonify({"messages": f"OpenAI API error\n{e}"}),
                 HTTPStatus.REQUEST_TIMEOUT
             )
         except Exception as e:
+
+            log = {"time": str(datetime.datetime.now()), "message": f"Error: {e}"}
+            write_log_in_txt(log, AnswerEvaluation.__name__)
+
             return make_response(
                 jsonify({"messages": f"error {e}"}),
                 HTTPStatus.INTERNAL_SERVER_ERROR
