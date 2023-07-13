dfc2971a5e0e8992a6e76254a567902399ce5b6d
yang-yeongjun <dasd412@naver.com>
2023-07-13 21:22:06 +0900

    feat & test : 1. generator에 로그 추가. 2. generator 프롬프트 생성 부분에서 프롬프트가 str로 변형되는 지 테스트하는 코드 작성

--- moview/modules/question_generator/init_question_generator.py
+++ moview/modules/question_generator/init_question_generator.py
@@ -1,3 +1,4 @@
+import datetime
 from langchain import LLMChain
 from langchain.prompts.chat import (
     SystemMessagePromptTemplate,
@@ -6,7 +7,7 @@ from langchain.prompts.chat import (
 )
 
 from moview.utils.data_manager import *
-from moview.utils.util import create_question_parser, remove_indent
+from moview.utils.util import create_question_parser, remove_indent, write_log_in_txt
 from typing import *
 
 QUESTION_COUNT: Final[int] = 10
@@ -25,11 +26,23 @@ class InitQuestionGenerator:
         Returns:
             List: 초기 질문 리스트
         """
+
+        log = {"time": str(datetime.datetime.now()), "message": "Start of InitQuestionGenerator"}
+        write_log_in_txt(log, InitQuestionGenerator.__name__)
+
         prompt = self.__make_init_question_prompt()
-        create_question_chain = LLMChain(llm=chat_manager.get_chat_model(),
-                                         prompt=prompt)
-        output = create_question_chain(str(QUESTION_COUNT))
-        return create_question_parser(output['text'])
+
+        log = {"time": str(datetime.datetime.now()),
+               "message": "InitQuestionGenerator made a prompt. prompt is : " + str(prompt)}
+        write_log_in_txt(log, InitQuestionGenerator.__name__)
+
+        result = self.__make_result_of_question_chain(chat_manager.get_chat_model(), prompt)
+
+        log = {"time": str(datetime.datetime.now()),
+               "message": "End of InitQuestionGenerator. result is : " + str(result)}
+        write_log_in_txt(log, InitQuestionGenerator.__name__)
+
+        return result
 
     def __make_init_question_prompt(self) -> ChatPromptTemplate:
         """
@@ -63,3 +76,10 @@ class InitQuestionGenerator:
             input_variables=["question_count"],
         )
         return prompt
+
+    def __make_result_of_question_chain(self, chat_model: ChatOpenAI, prompt: ChatPromptTemplate) -> list:
+        create_question_chain = LLMChain(llm=chat_model,
+                                         prompt=prompt)
+        output = create_question_chain(str(QUESTION_COUNT))
+
+        return create_question_parser(output['text'])
