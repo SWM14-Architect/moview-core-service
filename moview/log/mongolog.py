5d7f57bcc53bf0567db92143a14a29f087430900
hys <ssfic3380@gmail.com>
2023-07-19 01:56:54 +0900

    refactor: DB 정보를 환경변수에서 불러오도록 변경

--- moview/log/mongolog.py
+++ moview/log/mongolog.py
@@ -1,56 +1,61 @@
+import os
+import sys
 import logging
+import pymongo
 import datetime
 
 
 class MongoHandler(logging.Handler):
     def __init__(self, level=logging.NOTSET,
-                 host='localhost', port=27017, username=None, password=None, authMechanism='DEFAULT',
-                 databaseName='mongolog', collectionName='log', capped=True, size=100000, isCollectionDrop=False,
-                 instanceName='default'):
-        import pymongo
-        import platform
+                 databaseName='mongolog', collectionName='log',
+                 capped=True, size=100000, isCollectionDrop=False):
 
-        logging.Handler.__init__(self, level)  # // 부모 생성자 호출
+        # 부모 생성자 호출
+        logging.Handler.__init__(self, level)
 
-        self.conn = pymongo.MongoClient(host=host, port=port,  # // 몽고클라이언트를 만든다
-                                        username=username,
-                                        password=password,
-                                        authMechanism='DEFAULT')
-        self.db = self.conn.get_database(databaseName)  # // 데이터베이스를 가져온다
+        # MongoClient를 만들고, database를 가져온다
+        if sys.platform == 'darwin':
+            # MacOS
+            self.conn = pymongo.MongoClient(host=os.environ["CORE_DEV_DB_HOST"],
+                                            port=os.environ["CORE_DEV_DB_PORT"],
+                                            username=os.environ["CORE_DEV_DB_USERNAME"],
+                                            password=os.environ["CORE_DEV_DB_PASSWORD"])
+            self.db = self.conn.get_database(databaseName)
+        elif sys.platform == 'win32' or sys.platform == 'linux':
+            # Windows, linux
+            self.conn = pymongo.MongoClient(host=os.getenv("CORE_DEV_DB_HOST"),
+                                            port=os.getenv("CORE_DEV_DB_PORT"),
+                                            username=os.getenv("CORE_DEV_DB_USERNAME"),
+                                            password=os.getenv("CORE_DEV_DB_PASSWORD"))
+            self.db = self.conn.get_database(databaseName)
 
-        # // 데이터베이스 컬렉션을 가져온다
-        if collectionName in self.db.list_collection_names():  # // 존재한다면
+        # 데이터베이스 컬렉션을 가져온다
+        if collectionName in self.db.list_collection_names():  # 존재한다면
             if isCollectionDrop:
-                self.db.drop_collection(collectionName)  # // Drop하고
-                self.collection = self.create_collection(collectionName)  # // 다시 만든다
+                self.db.drop_collection(collectionName)  # Drop하고
+                self.collection = self.create_collection(collectionName)  # 다시 만든다
             else:
-                # // 가져온다
                 self.collection = self.db.get_collection(collectionName)
-        else:  # // 없다면
-            self.collection = self.create_collection(collectionName)  # // 만든다
-
-        self.hostName = platform.node()  # // 호스트이름을 저장한다
-        self.instanceName = instanceName  # // 인스턴스이름을 저장한다
+        else:
+            self.collection = self.create_collection(collectionName)
 
     def create_collection(self, collectionName):
-        # // 컬렉션이름으로 컬렉션을 만들고 리턴한다
+        # 컬렉션이름으로 컬렉션을 만들고 리턴한다
         return self.db.create_collection(collectionName,
-                                         capped=True,  # // 고정크기 컬렉션
-                                         size=10000000)  # // 컬렉션 최대크기지정(단위: bytes)
+                                         capped=True,  # 고정크기 컬렉션
+                                         size=10000000)  # 컬렉션 최대크기지정(단위: bytes)
 
     def emit(self, record):
         self.record = record
         document = \
             {
-                'when': datetime.datetime.now(),  # // 현재일시
-                'localhostName': self.hostName,  # // 로컬 호스트명
-                'localInstanceName': self.instanceName,  # // 로컬 인스턴스명
-                'fileName': record.filename,  # // 파일명
-                'processName': record.processName,  # // 프로세스명
-                'threadName': record.threadName,  # // 쓰레드명
-                'functionName': record.funcName,  # // 함수명
-                'levelNumber': record.levelno,  # // 로그레벨(ex. 10)
-                'levelName': record.levelname,  # // 로그레벨명(ex. DEBUG)
-                'message': record.msg,  # // 오류 메시지
+                'when': datetime.datetime.now(),  # 현재일시
+                'fileName': record.filename,  # 파일명
+                'processName': record.processName,  # 프로세스명
+                'threadName': record.threadName,  # 쓰레드명
+                'functionName': record.funcName,  # 함수명
+                'levelNumber': record.levelno,  # 로그레벨(ex. 10)
+                'levelName': record.levelname,  # 로그레벨명(ex. DEBUG)
+                'message': record.msg,  # 오류 메시지
             }
         self.collection.insert_one(document, bypass_document_validation=False)
