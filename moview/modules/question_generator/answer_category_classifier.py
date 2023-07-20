5e739b6a181dee4f8f829c20fd866df96295a7d4
yang-yeongjun <dasd412@naver.com>
2023-07-20 10:29:53 +0900

    feat & test : 대분류 분류기 생성 및 테스트 수행 테스트 파일은 테스트보단 사용 명세서임.

--- moview/modules/question_generator/answer_category_classifier.py
+++ moview/modules/question_generator/answer_category_classifier.py
@@ -11,17 +11,16 @@ from langchain.chat_models import ChatOpenAI
 from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 
 
-class AnswerFilter:
-
+class AnswerCategoryClassifier:
     def __init__(self):
         abs_path = os.path.dirname(os.path.abspath(__file__))
 
-        with open(abs_path + '/answer_filter_prompt.json', 'r') as f:
+        with open(abs_path + '/category_prompt.json', 'r') as f:
             data = json.load(f)
 
         self.prompt = data['prompt']
 
-    def check_answer_appropriate(self, job_group: str, question: str, answer: str) -> str:
+    def classify_category_of_answer(self, job_group: str, question: str, answer: str) -> str:
         prompt = ChatPromptTemplate(
             messages=[
                 SystemMessagePromptTemplate.from_template(
@@ -36,15 +35,13 @@ class AnswerFilter:
                     """
                     Previous interview question: {question}
 
-                    Candidate's response : {answer}
-
-                    """
-                )
+                    Candidate's response : {answer} 
+                    """)
             ],
             input_variables=["question", "answer"],
         )
 
-        llm = ChatOpenAI(temperature=0.3, model_name='gpt-3.5-turbo', verbose=True, streaming=True,
+        llm = ChatOpenAI(temperature=0.5, model_name='gpt-3.5-turbo', verbose=True, streaming=True,
                          callbacks=[StreamingStdOutCallbackHandler()])
         chain = LLMChain(llm=llm, prompt=prompt)
 
--- tests/modules/question_generator/test_answer_filter.py
+++ tests/modules/question_generator/test_answer_filter.py
@@ -1,5 +1,5 @@
 import unittest
-from unittest.mock import Mock, patch
+from unittest.mock import patch
 
 from moview.modules.question_generator.answer_filter import AnswerFilter
 
