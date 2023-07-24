49a6f96cd952e06de80dddb96986e0d144196893
hys <ssfic3380@gmail.com>
2023-07-24 16:54:21 +0900

    !HOTFIX: 권한 부족으로 인한 AWS CLI 설치 오류 해결

--- scripts/after_install/install_aws_cli.sh
+++ scripts/after_install/install_aws_cli.sh
@@ -5,8 +5,8 @@ if ! command -v aws &> /dev/null
 then
     echo "AWS CLI를 찾을 수 없습니다."
     echo "AWS CLI를 설치합니다..."
-    curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
-    unzip awscliv2.zip
+    sudo curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+    sudo unzip awscliv2.zip
     sudo ./aws/install
 else
     echo "AWS CLI가 이미 설치되어 있습니다."
