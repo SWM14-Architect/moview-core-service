6fe15088cf432e9afb9ecc2e97898dba0e011d1d
hys <ssfic3380@gmail.com>
2023-07-24 16:39:12 +0900

    test: mongo_handler와 mongo_logger를 위한 테스트 코드 변경
    - mongo_handler는 mongomock을 이용하여 실제 DB가 아닌 메모리에 데이터를 저장함으로써 collection 생성, document 저장 등을 테스트 하도록 변경
    - mongo_logger는 execution_trace_logger, error_logger, prompt_result_logger가 각 입력 케이스에 대해서 정상적으로 CustomLogRecord를 반환하는지 테스트 하도록 변경 중 (중요도가 낮기 때문에 이후에 더 추가하도록 할 예정)

--- tests/handlers/test_mongo_handler.py
+++ tests/handlers/test_mongo_handler.py
@@ -1,8 +1,12 @@
+import datetime
+import logging
+import os
 import unittest
 from unittest import TestCase, mock
-import logging
-from moview.handlers.mongo_handler import MongoHandler
+
 import mongomock
+
+from moview.handlers.mongo_handler import MongoHandler
 
 
 class TestMongoHandler(TestCase):
@@ -11,40 +15,37 @@ class TestMongoHandler(TestCase):
         # 테스트 시작 전에 임시 MongoDB 서버 생성
         self.db_name = 'test_database'  # 임시 데이터베이스 이름
         self.collection_name = 'test_collection'  # 테스트용 컬렉션 이름
-        self.client = mongomock.MongoClient()  # 가상의 MongoDB 클라이언트 생성
-        self.db = self.client[self.db_name]  # 가상의 MongoDB DB 생성
 
-        def create_collection(self, collection_name):
-            # 테스트 환경에서는 capped와 size 옵션 무시
-            return self.db.create_collection(collection_name)
-
-        # MongoHandler.create_collection() 메소드를 create_collection() 메소드를 mock으로 대체
-        with mock.patch.object(MongoHandler, 'create_collection', create_collection):
+        # mongomock은 capped와 size 옵션을 허용하지 않기 때문에, MongoHandler.create_collection() 메소드를 mock.patch로 대체
+        with mock.patch.object(MongoHandler, '_create_collection',
+                               lambda self, collection_name: self.db.create_collection(collection_name)):
             # 테스트용 MongoHandler 객체 생성
             self.handler = MongoHandler(level=logging.INFO,
-                                        database_name='test_database',
+                                        database_name=self.db_name,
                                         collection_name=self.collection_name)
 
-        # insert_one 메소드를 mock 객체로 대체 (훔쳐오기)
-        self.mock_insert = mock.Mock()
-        self.handler.collection.insert_one = self.mock_insert
-
-        # 테스트용 LogRecord 객체 생성
-        self.record = logging.LogRecord(name='test', level=logging.INFO, pathname=None,
-                                        lineno=0, msg='test message', args=None, exc_info=None)
+        # 테스트용 logger 생성
+        self.logger = logging.getLogger('test_logger')
+        self.logger.addHandler(self.handler)
+        self.logger.setLevel(logging.INFO)
 
     @mock.patch('pymongo.MongoClient', new=mongomock.MongoClient)
     def test_emit(self):
         # emit() 메소드 테스트
-        self.handler.emit(self.record)
+        self.logger.info('test message')  # info() 메소드가 handler.emit() 메소드를 호출합니다.
 
-        # insert_one 메소드가 호출되었는지 확인
-        self.mock_insert.assert_called_once()
+        # 컬렉션이 생성되었는지 테스트
+        self.assertIn(self.collection_name, self.handler.db.list_collection_names())
 
-        # insert_one에 전달된 인자가 예상한 로그 메시지를 포함하고 있는지 확인
-        args, _ = self.mock_insert.call_args  # insert_one에 전달된 인자를 가져옴
-        self.assertEqual(args[0]['level'], self.record.levelno)
-        self.assertEqual(args[0]['message'], self.record.msg)
+        # document가 삽입되었는지 테스트
+        documents = list(self.handler.db[self.collection_name].find())
+        self.assertEqual(len(documents), 1)  # 하나의 문서가 삽입 됐는지 확인
+        self.assertIsInstance(documents[0]['timestamp'], datetime.datetime)  # timestamp가 datetime 타입인지 확인
+        self.assertEqual(documents[0]['fileName'], os.path.basename(__file__))  # fileName이 현재 파일의 이름과 일치하는지 확인
+        self.assertEqual(documents[0]['className'], '(unknown class)') # className이 '(unknown class)'인지 확인 (기본 logger는 클래스명을 가져오지 못함)
+        self.assertEqual(documents[0]['functionName'], 'test_emit')  # functionName이 'test_emit'인지 확인
+        self.assertEqual(documents[0]['levelName'], 'INFO')
+        self.assertEqual(documents[0]['message'], 'test message')
 
 
 if __name__ == '__main__':
--- tests/loggers/test_mongo_logger.py
+++ tests/loggers/test_mongo_logger.py
@@ -1,9 +1,39 @@
 import unittest
+from unittest.mock import patch, MagicMock
+from moview.handlers.mongo_handler import MongoHandler
+from moview.loggers.mongo_logger import MongoLogger
+
 
 class TestMongoLogger(unittest.TestCase):
-    """
-    Test for MongoLogger
-    """
+    @patch('moview.loggers.mongo_logger.MongoHandler', autospec=True)
+    def setUp(self, mock_mongo_handler):
+        self.mock_mongo_handler = mock_mongo_handler
+        self.mongo_logger = MongoLogger()
+
+        self.execution_trace_logger = self.mongo_logger.execution_trace_logger
+        self.error_logger = self.mongo_logger.error_logger
+        self.prompt_result_logger = self.mongo_logger.prompt_result_logger
+
+    def test_execution_trace_logger(self):
+        """
+        execution_trace_logger에 각 입력 케이스에 대해서, 정상적으로 CustomLogRecord가 생성되는지 테스트
+        """
+        # 테스트용 CustomLogRecord 객체 생성
+        log_record = MagicMock()
+        log_record.levelname = 'INFO'
+        log_record.msg = 'test message'
+        log_record.args = None
+        log_record.kwargs = {'caller_frame': MagicMock()}
+
+        # execution_trace_logger 호출
+        self.execution_trace_logger(log_record)
+
+        # CustomLogRecord 생성 확인
+        self.mock_mongo_handler.assert_called_once_with(level=log_record.levelname,
+                                                        database_name='log',
+                                                        collection_name='executionTraceLogs')
+        self.mock_mongo_handler.return_value.emit.assert_called_once_with(log_record)
+
 
 if __name__ == "__main__":
     unittest.main()
