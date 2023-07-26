a3b32aab2cf31a8b9e9270ca9875fc8282dec4db
yang-yeongjun <dasd412@naver.com>
2023-07-26 20:43:55 +0900

    feat : 초기 입력 분석하는 클래스 프롬프트 작성 및 테스트 완료

--- moview/modules/initial_input/initial_input_analyzer.py
+++ moview/modules/initial_input/initial_input_analyzer.py
@@ -1,6 +1,24 @@
+import json
+import os
+
+from langchain import LLMChain
+from langchain.prompts.chat import (
+    SystemMessagePromptTemplate,
+    ChatPromptTemplate,
+    HumanMessagePromptTemplate
+)
+from langchain.chat_models import ChatOpenAI
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+
+
 class InitialInputAnalyzer:
     def __init__(self):
-        pass
+        abs_path = os.path.dirname(os.path.abspath(__file__))
+
+        with open(abs_path + '/initial_input_analyzer.json', 'r') as f:
+            data = json.load(f)
+
+        self.prompt = data['prompt']
 
     def analyze_initial_input(self, job_group: str, recruitment_announcement: str, cover_letter_question: str,
                               cover_letter_answer: str) -> str:
@@ -16,4 +34,26 @@ class InitialInputAnalyzer:
         Returns: i번째 자소서 답변, 문항 순서쌍에 대해 분석한 결과 문자열
 
         """
-        pass
+        prompt = ChatPromptTemplate(
+            messages=[
+                SystemMessagePromptTemplate.from_template(
+                    self.prompt.format(job_group=job_group)
+                ),
+                HumanMessagePromptTemplate.from_template(
+                    """
+                    job posting : {job_posting}
+                    cover letter question : {cover_letter_question}
+                    cover letter answer : {cover_letter_answer}    
+                    """)
+            ],
+            input_variables=["job_posting", "cover_letter_question", "cover_letter_answer"],
+        )
+
+        llm = ChatOpenAI(temperature=0.5, model_name='gpt-3.5-turbo', verbose=True, streaming=True,
+                         callbacks=[StreamingStdOutCallbackHandler()])
+        chain = LLMChain(llm=llm, prompt=prompt)
+
+        return chain.run({
+            "job_posting": recruitment_announcement,
+            "cover_letter_question": cover_letter_question,
+            "cover_letter_answer": cover_letter_answer})
---
+++ tests/modules/initial_input/test_inital_input_analyzer.py
@@ -0,0 +1,27 @@
+import unittest
+from moview.modules.initial_input.initial_input_analyzer import InitialInputAnalyzer
+
+
+def is_not_none_string(s):
+    return s is not None and isinstance(s, str)
+
+
+class TestInitialInputAnalyzer(unittest.TestCase):
+    def setUp(self) -> None:
+        self.analyzer = InitialInputAnalyzer()
+
+    def test_load_prompt(self):
+        self.assertTrue(is_not_none_string(self.analyzer.prompt))
+        print(self.analyzer.prompt.format(job_group="테스트 직군"))
+
+    # def test_analyze_initial_input(self):
+    #     job_group = "IT"
+    #     recruitment_announcement = "테스트 공고"
+    #     cover_letter_question = "테스트 자소서 문항"
+    #     cover_letter_answer = "테스트 자소서 답변"
+    #
+    #     analysis_result = self.analyzer.analyze_initial_input(job_group=job_group,
+    #                                                           recruitment_announcement=recruitment_announcement,
+    #                                                           cover_letter_question=cover_letter_question,
+    #                                                           cover_letter_answer=cover_letter_answer)
+    #     self.assertTrue(is_not_none_string(analysis_result))
