e362cea1ad9a21ce0e09f9aa6c97d5b7fce23056
yang-yeongjun <dasd412@naver.com>
2023-07-29 12:34:03 +0900

    remove : 기존 컨트롤러 삭제

--- moview/__init__.py
+++ moview/__init__.py
@@ -4,9 +4,7 @@ import string
 from flask import Flask
 from flask_restx import Api
 
-from moview.controller import evaluation_controller
-from moview.controller import generation_controller
-
+from moview.controller import answer_controller
 
 app = Flask(__name__)
 app.secret_key = ''.join(random.choice(string.ascii_letters) for i in range(20))
@@ -14,5 +12,4 @@ api = Api(app)
 
 app.config['JSON_AS_ASCII'] = False  # 한글 깨짐 방지
 
-api.add_namespace(evaluation_controller.api, '/api')
-api.add_namespace(generation_controller.api, '/api')
+api.add_namespace(answer_controller.api, '/api')
