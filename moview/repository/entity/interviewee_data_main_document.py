0a0021062df3bb2bd8d2a207cbfe91189fbb76a0
yang-yeongjun <dasd412@naver.com>
2023-07-31 18:03:56 +0900

    feat : 몽고 db 메인 다큐먼트, 서브 다큐먼트 엔티티 생성 (pydantic base model 사용)

---
+++ moview/repository/entity/interviewee_data_main_document.py
@@ -0,0 +1,44 @@
+from typing import List
+from pydantic import BaseModel
+from moview.repository.entity.interviewee_data_subdocument import IntervieweeInitialInputData, InitialInterviewAnalysis, \
+    InterviewQuestions, IntervieweeAnswerScore
+
+"""
+IntervieweeDataEntity 클래스를 정의할 때 Pydantic의 BaseModel을 사용하였습니다.
+Pydantic은 데이터 유효성 검사를 제공하는 Python 라이브러리이며, 편리하게 모델 클래스를 생성하는 데 사용됩니다.
+Pydantic 모델은 데이터 유효성 검사, JSON (de)serialization 등을 지원합니다.
+"""
+
+"""
+이 클래스에 명시적인 생성자(__init__ 메서드)가 정의되어 있지 않은 이유는 Pydantic의 BaseModel이 제공하는 기능을 이용하기 위해서입니다. 
+Pydantic의 BaseModel 클래스는 클래스를 정의할 때 선언한 필드에 대한 타입 검사, 필드의 기본값 설정, JSON 직렬화 및 역직렬화 등의 기능을 자동으로 제공합니다.
+"""
+
+"""
+임베딩 방식: 이 방식에서는 IntervieweeDataEntity를 메인 도큐먼트로 설정하고,
+ 그 안에 IntervieweeInitialInputData, InitialInterviewAnalysis, InterviewQuestions, IntervieweeAnswerScore를
+서브 도큐먼트로 둡니다. 이렇게 하면 모든 데이터는 하나의 도큐먼트에 저장되며, 동일한 session_id를 쉽게 공유할 수 있습니다.
+"""
+
+
+class IntervieweeDataEntity(BaseModel):
+    """
+    메인 다큐먼트
+    """
+    id: str  # 인터뷰 세션 id
+    initial_input_data: IntervieweeInitialInputData
+    initial_interview_analysis: InitialInterviewAnalysis
+    interview_questions: InterviewQuestions
+    interviewee_answer_score_list: List[IntervieweeAnswerScore]
+
+    def give_next_initial_question(self):
+        self.interview_questions.get_next_initial_question()
+
+    def save_followup_question(self, followup_question: str):
+        self.interview_questions.save_followup_question(followup_question)
+
+    def is_initial_questions_end(self) -> bool:
+        return self.interview_questions.is_initial_questions_end()
+
+    def is_followup_questions_end(self) -> bool:
+        return self.interview_questions.is_followup_questions_end()
---
+++ moview/repository/entity/interviewee_data_subdocument.py
@@ -0,0 +1,81 @@
+from typing import List
+from pydantic import BaseModel
+
+
+class IntervieweeInitialInputData(BaseModel):
+    """
+    인터뷰 세션을 생성할 때 입력받는 데이터를 저장하는 클래스입니다.
+    cover_letter_questions[i]에 대한 답변은 cover_letter_answers[i]에 저장됩니다.
+    """
+    interviewee_name: str
+    job_group: str
+    recruit_announcement: str
+    cover_letter_questions: List[str]
+    cover_letter_answers: List[str]
+
+
+class InitialInterviewAnalysis(BaseModel):
+    """
+    인터뷰 세션을 생성할 때 입력받은 자소서 문항에 대한 분석 결과를 저장하는 클래스입니다.
+    cover_letter_questions [i]에 대해 초기 분석 initial_interview_analysis[i]가 매핑됩니다.
+    """
+    initial_interview_analysis: List[str]
+
+
+class InterviewQuestions(BaseModel):
+    """
+    인터뷰 초기 질문과 꼬리질문을 관리하는 클래스입니다.
+    """
+    initial_question_list: List[str]
+    excluded_questions_for_giving_followup_question: List[str]  # 꼬리질문 출제에서 제외할 질문들 (초기 질문은 꼬리질문 출제에서 제외합니다.)
+    initial_question_index: int = 0  # 초기 질문 인덱스 i
+    followup_question_count: int = 0  # i 번째 초기 질문에서 출제한 꼬리질문 횟수
+    MAX_FOLLOWUP_QUESTION_COUNT: int = 3  # 꼬리질문 최대 횟수
+
+    def __init__(self, initial_question_list: List[str]):
+        """
+        BaseModel을 상속받는 클래스에서 __init__ 메서드를 정의하려면,
+        Pydantic의 검증 메커니즘을 따르기 위해 super().__init__()을 호출해야 합니다.
+        """
+        super().__init__(initial_question_list=initial_question_list)
+
+        self.excluded_questions_for_giving_followup_question = self.__exclude_initial_question(
+            initial_question_list=initial_question_list)
+
+    def __exclude_initial_question(self, initial_question_list: List[str]):
+        excluded_questions = []
+        for question in initial_question_list:
+            excluded_questions.append(question)
+        return excluded_questions
+
+    def get_next_initial_question(self):
+        if self.initial_question_index >= len(self.initial_question_list):
+            raise ValueError("초기 질문을 모두 사용하였습니다.")
+
+        self.initial_question_index += 1
+
+        # 꼬리질문 초기화
+        self.followup_question_count = 0
+
+    def save_followup_question(self, followup_question: str):
+        self.followup_question_count += 1
+        self.excluded_questions_for_giving_followup_question.append(followup_question)
+
+    def is_initial_questions_end(self) -> bool:
+        return self.initial_question_index == len(self.initial_question_list) - 1
+
+    def is_followup_questions_end(self) -> bool:
+        return self.followup_question_count == self.MAX_FOLLOWUP_QUESTION_COUNT
+
+
+class IntervieweeAnswerScore(BaseModel):
+    """
+    면접 지원자의 질문,
+    면접 지원자의 답변 ,
+    면접 지원자의 답변에 대한 대분류+중분류 문자열, (Ex: I think it is 대분류, especially 중분류)
+    면접 지원자 답변에 대한 점수.
+    """
+    question: str
+    answer: str
+    category_and_sub_category: str
+    score_of_answer: str
