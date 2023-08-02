17f398e8b798aa15b7ad7e939f66a0649abaabed
yang-yeongjun <dasd412@naver.com>
2023-08-02 14:31:33 +0900

    remove : vo 삭제 완료.

--- moview/service/__init__.py
+++ moview/service/__init__.py
@@ -1,4 +1,3 @@
 from moview.service.interviewee_input.interviewee_input_service import IntervieweeInputService
 from moview.service.interviewee_answer.interviewee_answer_service import IntervieweeAnswerService
-from moview.service.interviewee_data_vo import IntervieweeDataVO, IntervieweeInitialInputData
 from moview.service.interviewee_answer.interviewer_action_enum import InterviewerActionEnum
