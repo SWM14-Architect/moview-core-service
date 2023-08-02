5be4ba74ad96acba498de7985df93c2088d73003
yang-yeongjun <dasd412@naver.com>
2023-08-02 19:26:31 +0900

    feat : input api 포스트맨 테스트 성공. 리스폰스만 api 명세서에 맞게 DTO로 쏴주면 됨.

--- moview/controller/interviwee_input_controller.py
+++ moview/controller/interviwee_input_controller.py
@@ -1,12 +1,31 @@
-from flask import session, make_response, jsonify, request
+from flask import make_response, jsonify, request
 from flask_restx import Resource, Namespace
 from http import HTTPStatus
+from moview.service.interviewee_input.interviewee_input_service import IntervieweeInputService
 
 api = Namespace('input', description='input api')
 
 
 @api.route('/interviewee/input')
 class InputOfInterviewee(Resource):
+
     def post(self):
         session_id = request.cookies.get('session')
-        return make_response(jsonify({'message': session_id}), HTTPStatus.OK)
+        request_body = request.get_json()
+
+        interviewee_name = request_body['interviewee_name']
+        job_group = request_body['job_group']
+        recruit_announcement = request_body['recruit_announcement']
+        cover_letter_questions = request_body['cover_letter_questions']
+        cover_letter_answers = request_body['cover_letter_answers']
+
+        input_service = IntervieweeInputService()
+
+        saved_id = input_service.ask_initial_question_to_interviewee(
+            session_id=session_id, interviewee_name=interviewee_name, job_group=job_group,
+            recruit_announcement=recruit_announcement, cover_letter_questions=cover_letter_questions,
+            cover_letter_answers=cover_letter_answers
+        )
+
+        # todo dto로 리스폰스 바꾸기
+        return make_response(jsonify({'message': saved_id}), HTTPStatus.OK)
