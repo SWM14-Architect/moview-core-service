b4604657ce9fb992102d8bfd7bbdb625c75d636b
ssfic3380 <ssfic3380@naver.com>
2023-08-08 13:55:42 +0900

    fix: cors 오류 수정
    - /api/* 에 대한 요청만 허용
    - localhost:3000의 요청만 허용
    - supports_credentials를 통해서 쿠키나 인증 헤더를 포함하도록 허용

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from moview import app
+from flask_cors import CORS
+
+CORS(app, resources={r"/api/*": {"origins": "http://localhost:3000"}}, supports_credentials=True)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port=5005, debug=True)
