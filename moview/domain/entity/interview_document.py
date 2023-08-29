a56e1977bb846a8f735bc57bd992887b491e16af
JangSeHun <westreed@naver.com>
2023-08-29 20:46:54 +0900

    fix: typo in variable

--- moview/domain/entity/interview_document.py
+++ moview/domain/entity/interview_document.py
@@ -4,5 +4,5 @@ from pydantic import BaseModel
 
 class Interview(BaseModel):
     session_id: str
-    inital_input_data_id: Dict[str, Optional[str]] = None
+    initial_input_data_id: Dict[str, Optional[str]] = None
     initial_question_id: Dict[str, Optional[str]] = None
