47c9f6d8a18c0f17aef37584008b8c3eda113d91
JangSeHun <westreed@naver.com>
2023-08-31 20:25:01 +0900

    refactor: InputData Repository 메소드 세분화
    면접자의 이름으로 검색하는 메소드를 추가하고(for test),
    중복되는 코드를 또다른 메소드로 묶음.

--- moview/repository/input_data_repository.py
+++ moview/repository/input_data_repository.py
@@ -1,4 +1,4 @@
-from typing import Any, Dict, List
+from typing import Any, Dict, List, Optional
 
 from pymongo import MongoClient
 from pymongo.results import InsertOneResult
@@ -16,17 +16,13 @@ class InputDataRepository(metaclass=SingletonMeta):
         self.db = self.client[mongo_config.db_name]
         self.collection = self.db["input_data"]
 
-    def save(
-            self,
-            initial_input_data: InitialInputData,
-            cover_letter_list: List[CoverLetter]
-    ) -> InsertOneResult:
+    def save(self, initial_input_data: InitialInputData, cover_letter_list: List[CoverLetter]) -> InsertOneResult:
         for cover_letter in cover_letter_list:
             cover_letter_model = cover_letter.dict()
             cover_letter_document_id = str(self.collection.insert_one(cover_letter_model).inserted_id)
 
             # InitalInputData에 cover_letter_document_id를 추가함.
-            initial_input_data.interview_id_list.append({
+            initial_input_data.coverletter_id_list.append({
                 "#ref": self.collection.name,
                 "#id": cover_letter_document_id,
                 "#db": self.db.name
@@ -35,13 +31,27 @@ class InputDataRepository(metaclass=SingletonMeta):
         initial_input_data_model = initial_input_data.dict()
         return self.collection.insert_one(initial_input_data_model)
 
-    def find_by_object_id(self, object_id: str) -> Dict[str, Any]:
+    def find_cover_letter_by_object_id(self, coverletter_id: Dict[str, Optional[str]]) -> Dict[str, Any]:
+        # "#db"와 "#ref"를 이용해 document가 있는 collection에 접속함.
+        other_collection = self.client[coverletter_id["#db"]][coverletter_id["#ref"]]
+        return other_collection.find_one({"_id": ObjectId(coverletter_id["#id"])})
+
+    def find_input_data_by_object_id(self, object_id: str) -> Dict[str, Any]:
         document = self.collection.find_one({"_id": ObjectId(object_id)})
-        interview_id_list = document["interview_id_list"]
+        coverletter_id_list = document["coverletter_id_list"]
 
         document["interview_document"] = []
-        for interview_id in interview_id_list:
-            # "#db"와 "#ref"를 이용해 document가 있는 collection에 접속함.
-            other_collection = self.client[interview_id["#db"]][interview_id["#ref"]]
-            document["interview_document"].append(other_collection.find_one({"_id": ObjectId(interview_id["#id"])}))
+        for coverletter_id in coverletter_id_list:
+            coverletter_document = self.find_cover_letter_by_object_id(coverletter_id)
+            document["interview_document"].append(coverletter_document)
+        return document
+
+    def find_input_data_by_interviewee_name(self, interviewee_name: str) -> Dict[str, Any]:
+        document = self.collection.find_one({"interviewee_name": interviewee_name})
+        coverletter_id_list = document["coverletter_id_list"]
+
+        document["interview_document"] = []
+        for coverletter_id in coverletter_id_list:
+            coverletter_document = self.find_cover_letter_by_object_id(coverletter_id)
+            document["interview_document"].append(coverletter_document)
         return document
