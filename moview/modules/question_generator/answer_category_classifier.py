0031bb8fac2cbd31e3721473d2676338799dea72
yang-yeongjun <dasd412@naver.com>
2023-08-31 20:02:17 +0900

    feat : answer_service.py에 answerCategoryClassifier 로직 추가 및 그에 따른 기존 answer_category_classifier.py 파라미터 수정. 테스트 코드도 수정 완료.

--- moview/modules/question_generator/answer_category_classifier.py
+++ moview/modules/question_generator/answer_category_classifier.py
@@ -14,7 +14,7 @@ class AnswerCategoryClassifier:
         prompt_loader = PromptLoader()
         self.prompt = prompt_loader.load_prompt_json(AnswerCategoryClassifier.__name__)
 
-    def classify_category_of_answer(self, job_group: str, question: str, answer: str) -> str:
+    def classify_category_of_answer(self,question: str, answer: str) -> str:
         """
         질문과 답변, 직군을 입력 받아서 질문과 답변이 면접 유형 중 어느 대분류에 해당하는 지 분류하는 메서드.
         """
@@ -22,7 +22,7 @@ class AnswerCategoryClassifier:
         prompt = ChatPromptTemplate(
             messages=[
                 SystemMessagePromptTemplate.from_template(
-                    self.prompt.format(job_group=job_group)
+                    self.prompt.format()
                 ),
                 HumanMessagePromptTemplate.from_template(
                     """
--- tests/modules/question_generator/test_answer_category_classifier.py
+++ tests/modules/question_generator/test_answer_category_classifier.py
@@ -12,20 +12,19 @@ class TestAnswerCategoryClassifier(unittest.TestCase):
 
     def test_load_prompt(self):
         self.assertTrue(is_not_none_string(self.answer_category_classifier.prompt))
-        print(self.answer_category_classifier.prompt.format(job_group="테스트 직군"))
+        print(self.answer_category_classifier.prompt.format())
 
     @patch(
         'moview.modules.question_generator.answer_category_classifier.AnswerCategoryClassifier'
         '.classify_category_of_answer')
     def test_classify_category_of_answer(self, mock_method):
         # given
-        job_group = "테스트 직군"
         question = "테스트 질문"
         answer = "테스트 답변"
         mock_method.return_value = "classification result"
 
         # when
-        result = self.answer_category_classifier.classify_category_of_answer(job_group, question, answer)
+        result = self.answer_category_classifier.classify_category_of_answer(question, answer)
 
         # then
         self.assertIn("classification result", result)
