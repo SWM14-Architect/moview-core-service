9bb5d7d06395858e33dc9e02d98d53abd3cfd0cd
JangSeHun <westreed@naver.com>
2023-08-31 20:18:05 +0900

    refactor: CICD 환경설정 수정
    AWS Region에 대한 환경변수가 없으면 생성하는 코드가 추가됨.

--- scripts/before_install/set_environment_variables.sh
+++ scripts/before_install/set_environment_variables.sh
@@ -15,5 +15,14 @@ else
     echo ">>> MOVIEW_CORE_ENV 환경 변수가 설정되지 않았습니다. 'prod'로 설정하였습니다."
 fi
 
+echo ">>> AWS_DEFAULT_REGION 환경 변수 설정을 확인합니다."
+
+if ! grep -q 'export AWS_DEFAULT_REGION=' /home/ubuntu/.bashrc; then
+    echo 'export AWS_DEFAULT_REGION="ap-northeast-2"' >> /home/ubuntu/.bashrc
+    echo ">>> AWS_DEFAULT_REGION 환경 변수가 설정되지 않았습니다. 'ap-northeast-2'로 설정하였습니다."
+else
+    echo ">>> AWS_DEFAULT_REGION 환경 변수가 이미 설정되어 있습니다."
+fi
+
 source /home/ubuntu/.bashrc
 echo ">>> MOVIEW_CORE_ENV 환경 변수 설정이 적용되었습니다."
