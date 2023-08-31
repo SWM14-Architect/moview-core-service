1c5e0ffd901459c2737d35cb5ad105fdae83f4d3
JangSeHun <westreed@naver.com>
2023-08-31 20:21:56 +0900

    refactor: created_at 필드명 추가

--- moview/domain/entity/interview_document.py
+++ moview/domain/entity/interview_document.py
@@ -1,8 +1,10 @@
 from typing import Dict, Optional
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class Interview(BaseModel):
     session_id: str
     initial_input_data_id: Dict[str, Optional[str]] = None
-    initial_question_id: Dict[str, Optional[str]] = None
+    question_id: Dict[str, Optional[str]] = None
+    created_at: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
