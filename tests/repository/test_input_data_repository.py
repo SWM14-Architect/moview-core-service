af17e1d5e6e83037c7d37a61e0cef21070343738
JangSeHun <westreed@naver.com>
2023-08-31 20:25:57 +0900

    refactor: InputData Repository 테스트코드 추가
    추가된 메소드들을 테스트하기 위한 코드 추가.

--- tests/repository/test_input_data_repository.py
+++ tests/repository/test_input_data_repository.py
@@ -51,9 +51,9 @@ class TestInputDataRepository(unittest.TestCase):
         self.assertEqual(retrieved_document["company_name"], "test")
         self.assertEqual(retrieved_document["job_group"], "test")
         self.assertEqual(retrieved_document["recruit_announcement"], "test")
-        self.assertEqual(len(retrieved_document["interview_id_list"]), 3)
+        self.assertEqual(len(retrieved_document["coverletter_id_list"]), 3)
 
-    def test_find_by_object_id(self):
+    def test_find_input_data_by_object_id(self):
         # given
         test_initial_input_data_model = InitialInputData(
             interviewee_name="find_test", company_name="find_test", job_group="find_test", recruit_announcement="find_test"
@@ -68,11 +68,58 @@ class TestInputDataRepository(unittest.TestCase):
         test_document = self.repository.save(test_initial_input_data_model, test_cover_letter_model_list)
 
         # when
-        entity = self.repository.find_by_object_id(test_document.inserted_id)
+        retrieved_document = self.repository.find_input_data_by_object_id(test_document.inserted_id)
 
         # then
-        self.assertEqual(entity["interviewee_name"], "find_test")
-        self.assertEqual(entity["company_name"], "find_test")
-        self.assertEqual(entity["job_group"], "find_test")
-        self.assertEqual(entity["recruit_announcement"], "find_test")
-        self.assertEqual(len(entity["interview_id_list"]), 2)
+        self.assertEqual(retrieved_document["interviewee_name"], "find_test")
+        self.assertEqual(retrieved_document["company_name"], "find_test")
+        self.assertEqual(retrieved_document["job_group"], "find_test")
+        self.assertEqual(retrieved_document["recruit_announcement"], "find_test")
+        self.assertEqual(len(retrieved_document["coverletter_id_list"]), 2)
+
+    def test_find_input_data_by_interviewee_name(self):
+        # given
+        test_initial_input_data_model = InitialInputData(
+            interviewee_name="테스트면접자", company_name="find_test", job_group="find_test",
+            recruit_announcement="find_test"
+        )
+        test_cover_letter_model_list = [
+            CoverLetter(
+                coverletter_question=f"test",
+                coverletter_answer=f"test",
+                coverletter_analysis_result=f"test"
+            )
+        ]
+        test_document = self.repository.save(test_initial_input_data_model, test_cover_letter_model_list)
+
+        # when
+        retrieved_document = self.repository.find_input_data_by_interviewee_name("테스트면접자")
+
+        # then
+        self.assertEqual(retrieved_document["_id"], test_document.inserted_id)
+        self.assertEqual(retrieved_document["interviewee_name"], "테스트면접자")
+
+    def test_find_cover_letter_by_object_id(self):
+        # given
+        test_initial_input_data_model = InitialInputData(
+            interviewee_name="find_test", company_name="find_test", job_group="find_test",
+            recruit_announcement="find_test"
+        )
+        test_cover_letter_model_list = [
+            CoverLetter(
+                coverletter_question=f"test{i}",
+                coverletter_answer=f"test{i}",
+                coverletter_analysis_result=f"test{i}"
+            ) for i in range(2)
+        ]
+        test_document = self.repository.save(test_initial_input_data_model, test_cover_letter_model_list)
+
+        # when
+        saved_test_document = self.repository.find_input_data_by_object_id(str(test_document.inserted_id))
+        one_cover_letter_id = saved_test_document["coverletter_id_list"][0]
+        retrieved_document = self.repository.find_cover_letter_by_object_id(one_cover_letter_id)
+
+        # then
+        self.assertEqual(retrieved_document["coverletter_question"], "test0")
+        self.assertEqual(retrieved_document["coverletter_answer"], "test0")
+        self.assertEqual(retrieved_document["coverletter_analysis_result"], "test0")
