448c7ee862a40522e6c1d9af6aade5be0b4d5da1
yang-yeongjun <dasd412@naver.com>
2023-08-31 15:22:26 +0900

    feat : answer.py에 answerfilter 결괏값 추가!

--- moview/domain/entity/question_answer/answer.py
+++ moview/domain/entity/question_answer/answer.py
@@ -7,6 +7,7 @@ class Answer(BaseModel):
     content: Any
     category: str
     sub_category: str
+    filter_result: str = None  # Answer Filter 결괏값 저장됨
     evaluation: str  # 평가 모듈을 통해서 얻은 결과
     question_id: Dict[str, Optional[str]] = None
     created_at: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
