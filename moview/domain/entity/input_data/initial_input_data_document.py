d6754110f4fe38602257679d3020deadaa9f4f90
JangSeHun <westreed@naver.com>
2023-08-31 20:21:42 +0900

    refactor: InputData 문서의 필드명 수정
    자소서 문서에 대한 저장이므로 interview_id_list가 아닌
    coverletter_id_list로 수정함.

--- moview/domain/entity/input_data/initial_input_data_document.py
+++ moview/domain/entity/input_data/initial_input_data_document.py
@@ -1,6 +1,6 @@
 from typing import Dict, Optional, List
-
 from pydantic import BaseModel
+from datetime import datetime
 
 
 class InitialInputData(BaseModel):
@@ -8,4 +8,5 @@ class InitialInputData(BaseModel):
     company_name: str
     job_group: str
     recruit_announcement: str
-    interview_id_list: List[Dict[str, Optional[str]]] = []
+    coverletter_id_list: List[Dict[str, Optional[str]]] = []
+    created_at: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
