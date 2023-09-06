91f7fc235549b353412c58fa7de24504ed23f0d9
JangSeHun <westreed@naver.com>
2023-09-06 17:29:13 +0900

    refactor: Initial Question Giver 모듈 개선
    사용자 데이터 기반 질문과 일반 질문으로 나뉘어 생성하도록 변경됨.

--- moview/modules/input/initial_question_giver.py
+++ moview/modules/input/initial_question_giver.py
@@ -11,6 +11,7 @@ from langchain.prompts.chat import (
 from moview.utils.prompt_loader import PromptLoader
 from moview.environment.llm_factory import LLMModelFactory
 from moview.config.loggers.mongo_logger import prompt_result_logger
+from moview.utils.singleton_meta_class import SingletonMeta
 
 
 class InitialQuestionParseError(Exception):
@@ -20,42 +21,66 @@ class InitialQuestionParseError(Exception):
         super().__init__(self.message)
 
 
-class InitialQuestionGiver:
+class InitialQuestionGiver(metaclass=SingletonMeta):
 
-    def __init__(self):
-        prompt_loader = PromptLoader()
+    def __init__(self, prompt_loader: PromptLoader):
         self.prompt = prompt_loader.load_prompt_json(InitialQuestionGiver.__name__)
+        self.llm = LLMModelFactory.create_chat_open_ai(temperature=0.7)
 
-    def give_initial_questions(self, analysis_about_one_cover_letter: str, question_count: int) -> List[str]:
-        """
-
-        Args:
-            analysis_about_one_cover_letter: 면접 지원자 자소서 답변 한 개에 대한 분석 내용
-            question_count: 출제할 질문 개수
-
-        Returns: 분석 내용을 바탕으로 생성된 초기 질문 문자열 리스트 (question_count만큼 생성)
-
-        """
+    def give_initial_questions_by_input_data(
+            self, recruit_announcement: str, coverletter: str, question_count: int, exclusion_list: List[str] = None
+    ) -> List[str]:
+        exclusion_question = self.__create_exclusion_question_string(exclusion_list)
 
         prompt = ChatPromptTemplate(
             messages=[
                 SystemMessagePromptTemplate.from_template(
-                    self.prompt.format(question_count=question_count)
+                    self.prompt["create_question_by_input_data"].format(
+                        exclusion_question=exclusion_question,
+                        question_count=question_count
+                    )
                 ),
                 HumanMessagePromptTemplate.from_template(
-                    """
-                    analysis about cover letter : {analysis} 
-                    """)
+                    "양식을 지켜서 질문을 생성하세요.\n\n[회사의 모집공고]\n{recruit_announcement}\n\n[면접자의 자기소개서]\n{coverletter}"
+                )
             ],
-            input_variables=["analysis"],
+            input_variables=["recruit_announcement", "coverletter"],
         )
 
-        llm = LLMModelFactory.create_chat_open_ai(temperature=0.7)
-
-        chain = LLMChain(llm=llm, prompt=prompt)
-
+        chain = LLMChain(llm=self.llm, prompt=prompt)
         initial_questions_from_llm = chain.run({
-            "analysis": analysis_about_one_cover_letter})
+            "recruit_announcement": recruit_announcement,
+            "coverletter": coverletter
+        })
+
+        prompt_result_logger("initial question (input data) prompt result", prompt_result=initial_questions_from_llm)
+
+        parse_question = self.__parse_result_from_llm(initial_questions_from_llm)
+        # 파싱된 질문 개수가 출제할 질문 개수와 같으면, 파싱 성공으로 간주합니다. 파싱이 성공하면, 파싱된 질문 리스트를 반환합니다.
+        if len(parse_question) == question_count:
+            return parse_question
+        else:
+            raise InitialQuestionParseError()  # 파싱이 실패하면, InitialQuestionParseError를 발생시킵니다.
+
+    def give_initial_questions(self, job_group: str, question_count: int, exclusion_list: List[str] = None) -> List[str]:
+        exclusion_question = self.__create_exclusion_question_string(exclusion_list)
+
+        prompt = ChatPromptTemplate(
+            messages=[
+                SystemMessagePromptTemplate.from_template(
+                    self.prompt["create_question"].format(
+                        exclusion_question=exclusion_question,
+                        job_group=job_group,
+                        question_count=question_count
+                    )
+                ),
+                HumanMessagePromptTemplate.from_template("양식을 지켜서 질문을 생성하세요.")
+            ],
+            # input_variables=["analysis"],
+        )
+
+        chain = LLMChain(llm=self.llm, prompt=prompt)
+        initial_questions_from_llm = chain.predict()
 
         prompt_result_logger("initial question prompt result", prompt_result=initial_questions_from_llm)
 
@@ -66,7 +91,16 @@ class InitialQuestionGiver:
         else:
             raise InitialQuestionParseError()  # 파싱이 실패하면, InitialQuestionParseError를 발생시킵니다.
 
-    def __parse_result_from_llm(self, initial_questions_from_llm: str) -> List[str]:
+    @staticmethod
+    def __create_exclusion_question_string(exclusion_list: List[str]) -> str:
+        exclusion_question = ""
+        if exclusion_list is not None:
+            for idx, question in enumerate(exclusion_list):
+                exclusion_question += f"{idx+1}. {question}\n"
+        return exclusion_question
+
+    @staticmethod
+    def __parse_result_from_llm(initial_questions_from_llm: str) -> List[str]:
         """
 
         Args:
