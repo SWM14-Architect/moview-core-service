a79a47c9530e1042a963740aa140935642a0b492
JangSeHun <westreed@naver.com>
2023-09-07 15:33:50 +0900

    refactor: Interview Service 파라미터 추가
    초기질문 리스트를 받아와서 Document에 저장함.

--- moview/service/interview_service.py
+++ moview/service/interview_service.py
@@ -1,3 +1,5 @@
+from typing import List
+
 from moview.domain.entity.interview_session_document import InterviewSession
 from moview.repository.interview_repository import InterviewRepository
 from moview.utils.singleton_meta_class import SingletonMeta
@@ -8,8 +10,8 @@ class InterviewService(metaclass=SingletonMeta):
     def __init__(self, interview_repository: InterviewRepository):
         self.interview_repository = interview_repository
 
-    def create_interview_session(self, session_id: str) -> str:
+    def create_interview_session(self, session_id: str, initial_questions: List[str]) -> str:
         # session_id를 이용해 InterviewSession을 생성함.
-        interview_model = InterviewSession(user_id=session_id)
+        interview_model = InterviewSession(user_id=session_id, previous_question_content=initial_questions)
         interview_document = self.interview_repository.save_interview(interview_model)
         return str(interview_document.inserted_id)
