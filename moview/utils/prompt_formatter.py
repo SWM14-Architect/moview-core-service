50f8c36226de11314b771ac11218575fbc9b5e67
yang-yeongjun <dasd412@naver.com>
2023-09-10 10:52:17 +0900

    feat : 꼬리 질문 결괏값에 대한 파싱 코드 추가.

---
+++ moview/utils/prompt_formatter.py
@@ -0,0 +1,26 @@
+import re
+from moview.utils.singleton_meta_class import SingletonMeta
+from typing import Optional
+
+
+class PromptFormatter(metaclass=SingletonMeta):
+
+    @staticmethod
+    def parse_question(question: str) -> Optional[str]:
+        colon_pattern = ":"
+
+        if re.search(colon_pattern, question):  # 문자열에 : 이 있다면,
+            pattern = ":(.*?)(?=#)"  # : 뒤에 #이 나오기 전까지의 문자열을 추출
+            match = re.search(pattern, question)
+
+            if match:
+                return match.group(1).strip()
+            else:
+                return None
+        else:
+            pattern = r"\d+\.(.*?)(?=#)"  # 숫자. 뒤에 #이 나오기 전까지의 문자열을 추출
+            match = re.search(pattern, question)
+            if match:
+                return match.group(1).strip()
+            else:
+                return None
