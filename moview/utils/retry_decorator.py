9bb1cd343827ad3b647e7c0f61e6c67841c015e1
JangSeHun <westreed@naver.com>
2023-09-11 16:39:29 +0900

    feat: Retry Decorator 추가

---
+++ moview/utils/retry_decorator.py
@@ -0,0 +1,56 @@
+import functools
+import time
+import asyncio
+
+from moview.config.loggers.mongo_logger import execution_trace_logger
+
+
+class RetryExecutionError(Exception):
+
+    def __init__(self, message="Retry Execution Error"):
+        self.message = message
+        super().__init__(self.message)
+
+
+def retry(max_retries=3, retry_delay=2):
+    def decorator(func):
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            retries = 0
+
+            while retries < max_retries:
+                try:
+                    result = func(*args, **kwargs)
+                    return result  # 성공한 경우 결과를 반환합니다.
+                except Exception:
+                    retries += 1
+                    if retries < max_retries:
+                        execution_trace_logger(msg=f"{func.__name__} Execution Error: Retry ({retries}/{max_retries})...")
+                        time.sleep(retry_delay)
+                    else:
+                        execution_trace_logger(msg=f"{func.__name__} Execution Error: Failed due to excessive number of retries.")
+                        raise RetryExecutionError() # 재시도 횟수를 초과하면 예외를 다시 발생시킵니다.
+        return wrapper
+    return decorator
+
+
+def async_retry(max_retries=3, retry_delay=2):
+    def decorator(func):
+        @functools.wraps(func)
+        async def wrapper(*args, **kwargs):
+            retries = 0
+
+            while retries < max_retries:
+                try:
+                    result = await func(*args, **kwargs)
+                    return result  # 성공한 경우 결과를 반환합니다.
+                except Exception:
+                    retries += 1
+                    if retries < max_retries:
+                        execution_trace_logger(msg=f"{func.__name__} Execution Error: Retry ({retries}/{max_retries})...")
+                        await asyncio.sleep(retry_delay)  # 비동기 대기
+                    else:
+                        execution_trace_logger(msg=f"{func.__name__} Execution Error: Failed due to excessive number of retries.")
+                        raise RetryExecutionError() # 재시도 횟수를 초과하면 예외를 다시 발생시킵니다.
+        return wrapper
+    return decorator
