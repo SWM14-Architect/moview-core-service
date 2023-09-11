75d9cad124ac6f6687b425f8c4d325e47730b3c1
ssfic3380 <ssfic3380@naver.com>
2023-09-11 21:06:29 +0900

    feat: evaluation service 추가
    - 테스트 추가 필요

--- moview/domain/entity/question_answer/answer.py
+++ moview/domain/entity/question_answer/answer.py
@@ -1,4 +1,4 @@
-from typing import Any, Optional, Dict
+from typing import Any, Optional, Dict, List
 from pydantic import BaseModel
 from datetime import datetime
 
@@ -8,6 +8,6 @@ class Answer(BaseModel):
     category: str
     sub_category: str
     filter_result: str = None  # Answer Filter 결괏값 저장됨
-    evaluation: str  # 평가 모듈을 통해서 얻은 결과
+    evaluation: List[str] = None  # 평가 모듈을 통해서 얻은 결과
     question_id: Dict[str, Optional[str]] = None
     created_at: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
