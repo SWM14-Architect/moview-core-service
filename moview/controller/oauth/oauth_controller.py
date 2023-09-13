ffb1b167558f08b845506e29d8e4b8d9698cc76b
yang-yeongjun <dasd412@naver.com>
2023-09-13 16:19:32 +0900

    feat : oauth_controller.py 일단 완성

--- moview/controller/oauth/oauth_controller.py
+++ moview/controller/oauth/oauth_controller.py
@@ -10,6 +10,7 @@ from flask_jwt_extended import (
 from moview.config.container.container_config import ContainerConfig
 from moview.controller.oauth.oauth_controller_helper import OauthControllerHelper
 from moview.config.oauth.oauth_config import OAuthProvider
+from moview.config.oauth.oauth_config import OAuthConfigFactory
 
 api = Namespace('oauth', description='oauth api')
 
@@ -34,7 +35,7 @@ class KakaoOAuthController(Resource):
         response = make_response()
         access_token = create_access_token(identity=user_dict['id'])
         refresh_token = create_refresh_token(identity=user_dict['id'])
-        response.set_cookie('ignored', 'true')
+        response.set_cookie('logined', 'true')
         set_access_cookies(response, access_token)
         set_refresh_cookies(response, refresh_token)
 
@@ -42,8 +43,8 @@ class KakaoOAuthController(Resource):
 
 
 @api.route("/userinfo")
-@jwt_required()  # 인가 필요할 때 쓰임
 class UserInfoController(Resource):
+    @jwt_required()  # 인가 필요할 때 쓰임
     def get(self):
         # access token 을 이용해 db에서 user 정보를 가져옴
         user_id = get_jwt_identity()
@@ -52,3 +53,65 @@ class UserInfoController(Resource):
         user = user_service.get_user(user_id)
 
         return jsonify(user)
+
+
+@api.route('/oauth/url')
+class KakaoOAuthURLController(Resource):
+
+    def get(self):
+        """
+        Kakao OAuth URL 가져오기
+        """
+        client_id = OAuthConfigFactory.get_oauth_config(OAuthProvider.KAKAO).get_client_id()
+        redirect_uri = OAuthConfigFactory.get_oauth_config(OAuthProvider.KAKAO).get_redirect_uri()
+
+        return jsonify(
+            kakao_oauth_url="https://kauth.kakao.com/oauth/authorize?client_id=%s&redirect_uri=%s&response_type=code" \
+                            % (client_id, redirect_uri))
+
+
+@api.route("/oauth/refresh")
+class OAuthRefreshController(Resource):
+
+    def post(self):
+        #     refresh token을 인자로 받은 후,
+        #     kakao에서 access_token 및 refresh_token을 재발급.
+        refresh_token = request.get_json()['refresh_token']
+        result = OauthControllerHelper.refresh(refresh_token)
+        return jsonify(result)
+
+
+@api.route("/oauth/userinfo")
+class OAuthUserInfoController(Resource):
+
+    def post(self):
+        #     access token을 인자로 받은 후,
+        #     kakao에서 user 정보를 가져옴.
+        access_token = request.args.get('access_token')
+        oauth_helper = OauthControllerHelper(OAuthProvider.KAKAO)
+        result = oauth_helper.userinfo(bearer_token="Bearer " + access_token)
+        return jsonify(result)
+
+
+@api.route('token/refresh')
+class TokenRefreshController(Resource):
+
+    @jwt_required(refresh=True)
+    def post(self):
+        # refresh token을 이용해 access token 재발급
+        current_user_id = get_jwt_identity()
+        response = jsonify({'result': True})
+        access_token = create_access_token(identity=current_user_id)
+        set_access_cookies(response, access_token)
+        return response
+
+
+@api.route('token/remove')
+class TokenRemoveController(Resource):
+
+    def post(self):
+        # refresh token을 이용해 access token 재발급
+        response = jsonify({'result': True})
+        unset_jwt_cookies(response)
+        response.delete_cookie('logined')
+        return response
