5a78fd0348a34677882cd47fe5ed3311f1728aeb
hys <ssfic3380@gmail.com>
2023-09-14 15:20:44 +0900

    feat: controller에 logging 추가

--- moview/controller/evaluation_controller.py
+++ moview/controller/evaluation_controller.py
@@ -1,7 +1,9 @@
 from flask import make_response, jsonify, request
 from flask_restx import Resource, Namespace
 from http import HTTPStatus
+
 from moview.config.container.container_config import ContainerConfig
+from moview.config.loggers.mongo_logger import *
 from moview.utils.async_controller import async_controller
 
 api = Namespace('evaluation', description='evaluation api')
@@ -21,6 +23,8 @@ class EvaluationConstructor(Resource):
 
         # todo 로그인 추가 시 session_id를 user_id로 변경해야 함.
         results = await evaluation_service.evaluate_answers_of_interviewee(user_id=session_id, interview_id=interview_id)
+
+        execution_trace_logger("EVALUATION CONTROLLER: POST", user_id=session_id, interview_id=interview_id, results=results)
 
         return make_response(jsonify(
             {'message':
--- moview/controller/feedback_controller.py
+++ moview/controller/feedback_controller.py
@@ -1,7 +1,9 @@
 from flask import make_response, jsonify, request
 from flask_restx import Resource, Namespace
 from http import HTTPStatus
+
 from moview.config.container.container_config import ContainerConfig
+from moview.config.loggers.mongo_logger import *
 
 api = Namespace('feedback', description='feedback api')
 
@@ -22,6 +24,12 @@ class FeedbackConstructor(Resource):
         # todo 로그인 추가 시 session_id를 user_id로 변경해야 함.
         feedback_service.feedback(user_id=session_id, interview_id=interview_id, question_ids=question_ids,
                                   feedback_scores=feedback_scores)
+
+        execution_trace_logger("FEEDBACK CONTROLLER: POST",
+                               user_id=session_id,
+                               interview_id=interview_id,
+                               question_ids=question_ids,
+                               feedback_scores=feedback_scores)
 
         return make_response(jsonify(
             {'message': {
