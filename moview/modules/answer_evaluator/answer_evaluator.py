c2a076f7faf5cc4ade968471063118819e0c0ad5
hys <ssfic3380@gmail.com>
2023-09-14 15:09:12 +0900

    comment & refactor: 주석 수정 및 단순 위임 메소드 삭제

--- moview/modules/answer_evaluator/answer_evaluator.py
+++ moview/modules/answer_evaluator/answer_evaluator.py
@@ -25,14 +25,14 @@ class AnswerEvaluator(metaclass=SingletonMeta):
     async def evaluate_answer(self, question: str, answer: str) -> List[str]:
         """
 
-        면접자의 답변에 대해서 긍정적 평가와 부정적 평가를 반환하는 메소드
+        면접자의 답변에 대해서 평가하여 긍정적인 점과 개선해야 할 점을 반환하는 메소드
 
         Args:
             question: 면접자가 받았던 질문
             answer: 면접자의 답변
-            categories_ordered_pair: "I think it is (대분류) especially (중분류)" 문자열
 
-        Returns: 면접자의 답변에 대해 분석한 결과 문자열
+        Returns: 면접자의 답변에 대해 분석한 결과 리스트
+            List["긍정적인 점", "개선해야 할 점"]
 
         """
         prompt = ChatPromptTemplate(
@@ -60,12 +60,9 @@ class AnswerEvaluator(metaclass=SingletonMeta):
 
         prompt_result_logger("answer analyze prompt result", prompt_result=prompt_result)
 
-        parsed_result = self.__parse_result(result_string=prompt_result)
-        # 파싱된 질문 개수가 출제할 질문 개수와 같으면, 파싱 성공으로 간주합니다. 파싱이 성공하면, 파싱된 질문 리스트를 반환합니다.
+        parsed_result = PromptParser.parse_evaluation(prompt_result)
+        # 파싱된 평가의 개수가 2개(긍정적인 점, 개선해야 할 점)이며 비어 있지 않다면, 파싱 성공으로 간주합니다. 파싱이 성공하면, 파싱된 질문 리스트를 반환합니다.
         if len(parsed_result) == 2 and parsed_result[0] != "" and parsed_result[1] != "":
             return parsed_result
         else:
-            raise EvaluationParseError()  # 파싱이 실패하면, InitialQuestionParseError를 발생시킵니다.
-
-    def __parse_result(self, result_string: str) -> Optional[List[str]]:
-        return PromptParser.parse_evaluation(result_string)
+            raise EvaluationParseError()  # 파싱이 실패하면, EvaluationParseError 발생시킵니다.
