35f427dd40a08c865c25120ac78b11d295923696
JangSeHun <westreed@naver.com>
2023-09-22 16:34:39 +0900

    refactor: LLM Factory 파라미터 추가
    model_name도 입력받게 해서 상황에 맞게 모델을 자유자재로 쓸 수 있도록 함.
    기본값은 gpt-3.5-turbo로 설정. (8k token)

--- moview/environment/llm_factory.py
+++ moview/environment/llm_factory.py
@@ -10,10 +10,24 @@ class LLMModelFactory:
     """
     정적 메서드는 self를 받지 않으므로 인스턴스 속성에는 접근할 수 없습니다. 그래서 보통 정적 메서드는 인스턴스 속성, 인스턴스 메서드가 필요 없을 때 사용합니다.
     정적 메서드는 순수 함수(pure function)를 만들 때 사용합니다. 순수 함수는 부수 효과(side effect)가 없고 입력 값이 같으면 언제나 같은 출력 값을 반환합니다. 즉, 정적 메서드는 인스턴스의 상태를 변화시키지 않는 메서드를 만들 때 사용합니다.
+
+    model_name list             TPM     RPM
+    gpt-3.5-turbo	            90,000	3,500
+    gpt-3.5-turbo-0301	        90,000	3,500
+    gpt-3.5-turbo-0613	        90,000	3,500
+    gpt-3.5-turbo-16k	        180,000	3,500
+    gpt-3.5-turbo-16k-0613	    180,000	3,500
+    gpt-3.5-turbo-instruct	    250,000	3,000
+    gpt-3.5-turbo-instruct-0914	250,000	3,000
+    gpt-4	                    10,000	200
+    gpt-4-0314	                10,000	200
+    gpt-4-0613                  10,000	200
     """
 
     @staticmethod
-    def create_chat_open_ai(temperature: float, request_timeout: int = 60) -> ChatOpenAI:
+    def create_chat_open_ai(model_name: str = "gpt-3.5-turbo",
+                            temperature: float = 0.5,
+                            request_timeout: int = 60) -> ChatOpenAI:
         return ChatOpenAI(openai_api_key=EnvironmentLoader.getenv(OPENAI_API_KEY_PARAM),
-                          temperature=temperature, model_name='gpt-3.5-turbo-16k',
+                          temperature=temperature, model_name=model_name,
                           verbose=False, streaming=False, request_timeout=request_timeout)
