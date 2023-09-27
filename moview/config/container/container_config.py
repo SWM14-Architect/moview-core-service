e621742d8b0eec4f1b7df7988a914e9585020963
yang-yeongjun <dasd412@naver.com>
2023-09-27 10:48:27 +0900

    feat : light mode에 필요한 컴포넌트들 DI 컨테이너에 추가

--- moview/config/container/container_config.py
+++ moview/config/container/container_config.py
@@ -2,6 +2,7 @@ from moview.config.db.mongo_config import MongoConfig
 from moview.modules.input import InputAnalyzer, InitialQuestionGiver
 from moview.modules.question_generator import FollowUpQuestionGiver
 from moview.modules.answer_evaluator import AnswerEvaluator
+from moview.modules.light.light_question_giver import LightQuestionGiver
 from moview.repository.input_data.input_data_repository import InputDataRepository
 from moview.repository.interview_repository import InterviewRepository
 from moview.repository.question_answer.question_answer_repository import QuestionAnswerRepository
@@ -10,6 +11,7 @@ from moview.service.input_data_service import InputDataService
 from moview.service.answer_service import AnswerService
 from moview.service.evaluation_service import EvaluationService
 from moview.service.feedback_service import FeedbackService
+from moview.service.light_mode_service import LightModeService
 from moview.utils.prompt_loader import PromptLoader
 from moview.repository.user.user_repository import UserRepository
 from moview.service.user_service import UserService
@@ -26,6 +28,7 @@ class ContainerConfig:
         self.initial_input_analyzer = InputAnalyzer(prompt_loader=self.prompt_loader)
         self.followup_question_giver = FollowUpQuestionGiver(prompt_loader=self.prompt_loader)
         self.answer_evaluator = AnswerEvaluator(prompt_loader=self.prompt_loader)
+        self.light_question_giver = LightQuestionGiver(prompt_loader=self.prompt_loader)
 
         # Repository
         self.interview_repository = InterviewRepository(mongo_config=self.mongo_config)
@@ -54,3 +57,9 @@ class ContainerConfig:
             answer_evaluator=self.answer_evaluator
         )
         self.feedback_service = FeedbackService(question_answer_repository=self.question_answer_repository)
+
+        self.light_mode_service = LightModeService(
+            input_data_repository=self.input_data_repository,
+            question_answer_repository=self.question_answer_repository,
+            light_question_giver=self.light_question_giver
+        )
