b17e47fc3877914afcdc85bdc0ca82422213af35
yang-yeongjun <dasd412@naver.com>
2023-10-05 18:31:16 +0900

    hot-fix : decorator 환경 변수 수정

--- moview/utils/timing_decorator.py
+++ moview/utils/timing_decorator.py
@@ -19,7 +19,7 @@ def api_timing_decorator(f):
         message = f'{class_name}.{f.__name__} function took {str((end_time - start_time) * 1000.0)} ms'
 
         # 로컬 개발 환경에서는 콘솔에 출력하고, 그 외에는 로그에 기록합니다.
-        if EnvironmentLoader.getenv("MOVIEW_CORE_ENV") == "local":
+        if EnvironmentLoader.get_local_env("MOVIEW_CORE_ENV") == "local":
             print(message)
         else:
             execution_trace_logger(elapsed=message)
