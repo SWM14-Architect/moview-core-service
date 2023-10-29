97f7ee7b6d7103fd3b0ea246fbb7e5142dd20fe6
JangSeHun <westreed@naver.com>
2023-10-29 12:46:26 +0900

    fix: pydantic의 Field를 활용하여 created_at 버그 해결

--- moview/domain/entity/input_data/initial_input_data_document.py
+++ moview/domain/entity/input_data/initial_input_data_document.py
@@ -1,5 +1,5 @@
 from typing import Dict, Optional, List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 
 
@@ -10,4 +10,4 @@ class InitialInputData(BaseModel):
     keyword: Optional[str]
     recruit_announcement: Optional[str]  # light mode 인 경우 None
     coverletter_id_list: List[Dict[str, Optional[str]]] = []
-    created_at: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    created_at: str = Field(default_factory=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
--- moview/domain/entity/interview_document.py
+++ moview/domain/entity/interview_document.py
@@ -1,5 +1,5 @@
 from typing import Dict, Optional, List
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from datetime import datetime
 
 
@@ -11,4 +11,4 @@ class Interview(BaseModel):
     initial_input_data_id: Dict[str, Optional[str]] = None
     previous_question_content: List[str] = []  # 꼬리 질문에서 출제되었던 질문들을 제외하기 위함. todo 초기 질문은 input api 할 때 처리해야 함.
     question_id_list: List[Dict[str, Optional[str]]] = []  # 마이 페이지 구현 시, 한꺼번에 조회하기 위해 만든 id 리스트
-    created_at: str = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    created_at: str = Field(default_factory=lambda: datetime.now().strftime('%Y-%m-%d %H:%M:%S'))
